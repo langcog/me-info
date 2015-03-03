818a7aaf391a9177dcc276f52bd419e504341f1e
rosemschneider <roseschneider@sr13-fb1fc86325.stanford.edu>
2015-03-03 10:58:53 -0800

    set timeout to 8s

--- me-info_adults.js
+++ me-info_adults.js
@@ -266,9 +266,9 @@ var experiment = {
 						experiment.experimentTest();
 					}
 
-				}, 5000); //do we want this to last for a fixed amount of time, or have it end after the sound files are finished?
+				}, 8000); //do we want this to last for a fixed amount of time, or have it end after the sound files are finished?
 			}, 1000);
-		}, 5000); //do we want this to last for a fixed amount of time, or have it end after the sound files are finished?
+		}, 8000); //do we want this to last for a fixed amount of time, or have it end after the sound files are finished?
 	},
 
 	test: function(counter, total, words, imgs, type) {
