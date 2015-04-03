ca04d1bc700986cb020e40be54eab3c88f79ce2e
Dan Yurovsky <dyurovsky@gmail.com>
2015-04-03 13:20:31 -0700

    updated to multiple namings

--- spriteData.js
+++ spriteData.js
@@ -1,112 +1,222 @@
 var spriteData = {
 	look_cup: {
 		start: 0,
-		onset: 1.578,
-		end: 3.205
+		onset: 1.878,
+		end: 3.505
 	},
 	find_cup: {
-		start: 4.205,
-		onset: 6.180,
-		end: 6.763
+		start: 4.505,
+		onset: 6.469,
+		end: 7.106
 	},
 	look_flower: {
-		start: 7.763,
-		onset: 9.288,
-		end: 11.201
+		start: 7.106,
+		onset: 9.614,
+		end: 11.604
 	},
 	find_flower: {
-		start: 12.201,
-		onset: 14.083,
-		end: 14.731
+		start: 12.604,
+		onset: 14.410,
+		end: 15.027
 		},
 	look_shoe: {
-		start: 15.731,
-		onset: 17.205,
-		end: 19.043
+		start: 16.027,
+		onset: 17.515,
+		end: 19.386
 	},
 	find_shoe: {
-		start: 20.043,
-		onset: 22.025,
-		end: 22.567
+		start: 20.386,
+		onset: 22.392,
+		end: 22.929
 	},
 	look_fenam: {
-		start: 23.567,
-		onset: 25.119,
-		end: 27.156
+		start: 23.929,
+		onset: 25.457,
+		end: 27.527
 	},
 	find_fenam: {
-		start: 28.156,
-		onset: 30.099,
-		end: 30.745
+		start: 28.527,
+		onset: 30.413,
+		end: 31.130
 	},
 	look_jic: {
-		start: 31.745,
-		onset: 33.325,
-		end: 35.069
+		start: 32.130,
+		onset: 33.638,
+		end: 35.389
 	},
 	find_jic: {
-		start: 36.069,
-		onset: 38.044,
-		end: 38.546
+		start: 36.389,
+		onset: 38.335,
+		end: 38.872
 	},
 	look_kumo: {
-		start: 39.546,
-		onset: 41.148,
-		end: 43.141
+		start: 39.872,
+		onset: 41.460,
+		end: 43.470
 	},
 	find_kumo: {
-		start: 44.141,
-		onset: 46.121,
-		end: 46.685
+		start: 44.470,
+		onset: 46.436,
+		end: 46.993
 	},
 	look_loga: {
-		start: 47.685,
-		onset: 49.342,
-		end: 51.209
+		start: 47.993,
+		onset: 49.660,
+		end: 51.551
 	},
 	find_loga: {
-		start: 52.209,
-		onset: 54.265,
-		end: 54.831
+		start: 52.551,
+		onset: 54.577,
+		end: 55.154
 	},
 	look_pel: {
-		start: 55.831,
-		onset: 57.352,
-		end: 58.923
+		start: 56.154,
+		onset: 57.662,
+		end: 59.234
 	},
 	find_pel: {
-		start: 59.923,
-		onset: 61.196,
-		end: 62.338
+		start: 60.234,
+		onset: 62.240,
+		end: 62.658
 	},
 	look_rudd: {
-		start: 63.338,
-		onset: 64.913,
-		end: 66.632
+		start: 63.658,
+		onset: 65.225,
+		end: 66.957
 	},
 	find_rudd: {
-		start: 67.632,
-		onset: 69.625,
-		end: 70.043
+		start: 67.957,
+		onset: 69.903,
+		end: 70.380
 	},
 	look_vot: {
-		start: 71.043,
-		onset: 72.642,
-		end: 74.199
+		start: 71.380,
+		onset: 72.948,
+		end: 74.600
 	},
 	find_vot: {
-		start: 75.199,
-		onset: 77.230,
-		end: 77.703
+		start: 75.600,
+		onset: 77.566,
+		end: 77.924
 	},
 	look_truck: {
-		start: 78.703,
-		onset: 81.201,
-		end: 82.153
+		start: 78.924,
+		onset: 80.512,
+		end: 82.402
 	},
 	find_truck: {
-		start: 83.153,
-		onset: 85.150,
-		end: 85.691
+		start: 83.402,
+		onset: 85.408,
+		end: 85.985
+	},
+	one_cup: {
+		start: 86.985,
+		onset: 89.150,
+		end: 90.782
+	},
+	this_cup: {
+		start: 91.782,
+		onset: 93.569,
+		end: 95.240
+	},
+	one_flower: {
+		start: 96.240,
+		onset: 98.405,
+		end: 100.356
+	},
+	this_flower: {
+		start: 101.356,
+		onset: 103.063,
+		end: 105.232
+		},
+	one_shoe: {
+		start: 106.232,
+		onset: 108.377,
+		end: 110.100
+	},
+	this_shoe: {
+		start: 111.100,
+		onset: 112.895,
+		end: 114.766
+	},
+	one_fenam: {
+		start: 115.766,
+		onset: 118.230,
+		end: 120.499
+	},
+	this_fenam: {
+		start: 121.499,
+		onset: 123.225,
+		end: 125.674
+	},
+	one_jic: {
+		start: 126.674,
+		onset: 129.276,
+		end: 131.167
+	},
+	this_jic: {
+		start: 132.167,
+		onset: 133.934,
+		end: 136.063
+	},
+	one_cumo: {
+		start: 137.063,
+		onset: 139.348,
+		end: 141.378
+	},
+	this_cumo: {
+		start: 142.378,
+		onset: 144.144,
+		end: 146.314
+	},
+	one_loga: {
+		start: 147.314,
+		onset: 149.499,
+		end: 151.668
+	},
+	this_loga: {
+		start: 152.668,
+		onset: 154.296,
+		end: 156.346
+	},
+	one_pel: {
+		start: 157.346,
+		onset: 159.550,
+		end: 161.202
+	},
+	this_pel: {
+		start: 162.202,
+		onset: 164.009,
+		end: 165.780
+	},
+	one_rudd: {
+		start: 166.780,
+		onset: 168.965,
+		end: 170.816
+	},
+	this_rudd: {
+		start: 171.816,
+		onset: 173.563,
+		end: 175.473
+	},
+	one_vot: {
+		start: 176.473,
+		onset: 178.777,
+		end: 180.449
+	},
+	this_vot: {
+		start: 181.449,
+		onset: 183.276,
+		end: 185.087
+	},
+	one_truck: {
+		start: 186.087,
+		onset: 188.192,
+		end: 190.123
+	},
+	this_truck: {
+		start: 191.123,
+		onset: 192.830,
+		end: 194.900
 	}
 };
